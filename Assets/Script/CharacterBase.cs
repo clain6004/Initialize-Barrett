f8e8db25c30ece2df51bcb2f040ab78e12d516c7
clain6004 <kirisame6004@gmail.com>
2018-09-15 08:54:30 +0900

    Initialize Barrettスクリプトテスト インターフェーステスト

---
+++ Assets/Script/CharacterBase.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface CharacterSystem
+{
+
+    void Motor(Vector3 charactervector);
+
+}
+
+public class CharacterControllers : CharacterSystem
+{
+
+    GameObject Character;
+
+    Vector3 CharacterVector;
+
+    float Speed;
+
+    float Gravity;
+
+    float VectorY;
+
+    float JumpTime;
+
+    float JumpDownTime;
+
+    float JumpTimeUpdate;
+
+    float JumpPower;
+
+    float JumpupdateMax;
+
+    float DashTime;
+
+    float DashDownTime;
+
+    float DashTimeUpdate;
+
+    float DashPower;
+
+    float DashupdateMax;
+
+    bool JumpCoolBool;
+
+    CharacterController characterController;
+
+    public CharacterControllers(GameObject character,float speed,float gravity,float jumpPower,float jumptimemax,float jumpdowntime,float jumptimeupdate)
+    {
+
+        Character = character;
+
+        Speed = speed;
+
+        Gravity = gravity;
+
+        JumpDownTime=jumpdowntime;
+
+        JumpPower=jumpPower;
+
+        JumpupdateMax=jumptimemax;
+
+        JumpTimeUpdate = jumptimeupdate;
+
+
+
+    }
+
+    public void Motor(Vector3 charactervector)
+    {
+
+        CharacterVector = charactervector;
+
+        CharacterVector.y = VectorY;
+
+        Character.GetComponent<CharacterController>().Move(CharacterVector*Time.deltaTime);
+
+        VectorY -= Gravity * Time.deltaTime;
+
+    }
+
+    public void Jump()
+    {
+
+        if (Input.GetButton("Jump"))
+        {
+
+            if (JumpCoolBool == false)
+            {
+
+                if (JumpTime <= JumpupdateMax)
+                {
+
+                    JumpTime += JumpTimeUpdate * Time.deltaTime;
+
+                    VectorY = JumpPower;
+
+                }
+                else
+                {
+
+                    JumpCoolBool = true;
+
+                }
+
+            }
+
+        }
+
+        if (JumpCoolBool)
+        {
+
+            if (JumpTime <= 0)
+            {
+
+                JumpCoolBool = false;
+
+            }
+            else
+            {
+
+                JumpTime -= JumpDownTime * Time.deltaTime;
+
+            }
+
+        }
+
+    }
+
+    public void Dash()
+    {
+
+        if (Input.GetButton("Jump"))
+        {
+
+            if (JumpCoolBool == false)
+            {
+
+                if (JumpTime <= JumpupdateMax)
+                {
+
+                    JumpTime += JumpTimeUpdate * Time.deltaTime;
+
+                    VectorY = JumpPower;
+
+                }
+                else
+                {
+
+                    JumpCoolBool = true;
+
+                }
+
+            }
+
+        }
+
+        if (JumpCoolBool)
+        {
+
+            if (JumpTime <= 0)
+            {
+
+                JumpCoolBool = false;
+
+            }
+            else
+            {
+
+                JumpTime -= JumpDownTime * Time.deltaTime;
+
+            }
+
+        }
+
+    }
+
+}
---
+++ Assets/Script/CharacterInput.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterInput : MonoBehaviour {
+
+    [SerializeField]
+    float Speed;
+
+    [SerializeField]
+    float JumpPower;
+
+    [SerializeField]
+    float JumpTimeMax;
+
+    [SerializeField]
+    float JumpTimeUpdate;
+
+    [SerializeField]
+    float JumpDownTime;
+
+    [SerializeField]
+    float Gravity;
+
+    bool Isground;
+
+    CharacterControllers characterControllers;
+
+    Vector3 CharacterVector;
+
+    // Use this for initialization
+    void Start () {
+
+        characterControllers = new CharacterControllers(this.gameObject,Speed,Gravity,JumpPower,JumpTimeMax,JumpDownTime,JumpTimeUpdate);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        CharacterVector = new Vector3(Input.GetAxis("Horizontal") * Speed,0, Input.GetAxis("Vertical") * Speed);
+
+        characterControllers.Motor(CharacterVector);
+
+        characterControllers.Jump();
+
+	}
+}
